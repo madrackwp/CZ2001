cd4377153cac17bc9da65fffda976f539050bec3
madrackwp <gohweipin98@gmail.com>
2020-09-01 14:31:17 +0800

    Added the brute force algo

--- bruteForce.py
+++ bruteForce.py
@@ -1,1 +1,11 @@
-//This will be our code for bruteforce algo
+text = "ATTAAAGGTTTATACCTTCCCAGGTAACAAACCAACCAACTTTCGATCTCTTGTAGATCTGTTCTCGCTA"
+pattern = "TTTATACCTTCC"
+patternMatch = 0
+indexLocale = null
+
+for index in text:
+    if (index == pattern[patternMatch]):
+        patternMatch += 1
+    if (patternMatch == len(pattern)):
+        print("found!")
+        patternMatch = 0
