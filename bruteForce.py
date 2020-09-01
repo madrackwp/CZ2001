3106c2b07b27c1744c8418170a39fe5e030d7c87
madrackwp <gohweipin98@gmail.com>
2020-09-01 16:16:59 +0800

    Unfinished bruteforce

--- bruteForce.py
+++ bruteForce.py
@@ -1,11 +1,18 @@
 text = "ATTAAAGGTTTATACCTTCCCAGGTAACAAACCAACCAACTTTCGATCTCTTGTAGATCTGTTCTCGCTA"
 pattern = "TTTATACCTTCC"
 patternMatch = 0
-indexLocale = null
+indexLocale = [None]
+indexTemp = None
 
 for index in text:
     if (index == pattern[patternMatch]):
+        if (patternMatch == 0):
+            indexTemp = text.index
         patternMatch += 1
     if (patternMatch == len(pattern)):
         print("found!")
         patternMatch = 0
+        indexLocale.append(indexTemp)
+    indexTemp = None
+
+print(indexLocale)
