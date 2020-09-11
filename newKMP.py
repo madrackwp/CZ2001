d3abbc2e13bdff614d11b25ff33c7c003c2a4e1c
Luke <tankengkailuke@gmail.com>
2020-09-12 03:18:49 +0800

    Update

--- newKMP.py
+++ newKMP.py
@@ -1,10 +1,4 @@
-# https://www.youtube.com/watch?v=4jY57Ehc14Y&ab_channel=WebofStories-LifeStoriesofRemarkablePeople
-# https://www.geeksforgeeks.org/python-program-for-kmp-algorithm-for-pattern-searching-2/
-
-import timeit
-
-# find longest prefix that is also a suffix
-
+import numpy as np
 
 def computeLPSArray(pat, M, lps):
     length = 0
@@ -28,46 +22,33 @@ def computeLPSArray(pat, M, lps):
                 lps[i] = 0
                 i += 1
 
+def computeFailureTable(characters, pattern, lps, FT): 
+    j = 0
+    for i in characters: 
+        l = 0
+        while l < length: 
+            if pattern[lps[l]] == i: 
+                FT[j][l] = lps[l] + 1
+            else: 
+                if lps[l] == 0: 
+                    FT[j][l] = 0
+                else: 
+                    FT[j][l] = FT[j][lps[l]-1]
+            l += 1
+        j += 1
+
+def printFailureTable(characters, FT): 
+    for i in range(len(characters)): 
+        print(characters[i], end=" ")
+        print(FT[i])
 
 text = "TTTATACCTTCCATTAAAGGTTTATACCTTCCCAGGTAACAAACCAACCAACTTTCGATCTCTTGTAGATCTGTTCTCTAAACGAACTTTAAAATCTGTGTGGCTGTCACTCGGCTGCATGCTTAGTGCACTCACGCAGTATAATTAATAACTAATTACTGTCGTTGACAGGACACGAGTAACTCGTCTATCTTCTGCAGGCTGCTTACGGTTTCGTCCGTGTTGCAGCCGATCATCAGCACATCTAGGTTTCGTCCGGGTGTGACCGAAAGGTAAGATGGAGAGCCTTGTCCCTGGTTTCAACGAGAAAACACACGTCCAACTCAGTTTGCCTGTTTTACAGGTTCGCGACGTGCTCGTACGTGGCTTTGGAGACTCCGTGGAGGAGGTCTTATCAGAGGCACGTCAACATCTTAAAGATGGCACTTGTGGCTTAGTAGAAGTTGAAAAAGGCGTTTTGCCTCAACTTGAACAGCCCTATGTGTTCATCAAACGTTCGGATGCTCGAACTGCACCTCATGGTCATGTTATGGTTGAGCTGGTAGCAGAACTCGAAGGCATTCAGTACGGTCGTAGTGGTGAGACACTTGGTGTCCTTGTCCCTCATGTGGGCGAAATACCAGTGGCTTACCGCAAGGTTCTTCTTCGTAAGAACGGTAATAAAGGAGCTGGTGGCCATAGTTACGGCGCCGATCTAAAGTCATTTGACTTAGGCGACGAGCTTGGCACTGATCCTTATGAAGATTTTCAAGAAAACTGGAACACTAAACATAGCAGTGGTGTTACCCGTGAACTCATGCGTGAGCTTAACGGAGGGGCATACACTCGCTATTTATACCTTCC"
 pattern = "TTTATACCTTCC"
-# pattern = "DAD" (to test failed case)
-# create a list to record the indexes where matches are found
-
-
-def KMPAlgo(text, pattern):
-    indexList = []
-    textLength = len(text)
-    patternLength = len(pattern)
-    # create an array for LPS
-    lps = [0]*patternLength
-    # compute LPS first
-    computeLPSArray(pattern, patternLength, lps)
-    a = 0
-    b = 0
-    while a < textLength:
-        if text[a] == pattern[b]:
-            # match the pattern with the text, and the increase the counter a and b
-            a += 1
-            b += 1
-        else:
-            if b != 0:
-                # equate b to lps[b-1], so we do not need to check from the beginning if there is an lps
-                b = lps[b-1]
-            else:
-                a += 1
-        if b == patternLength:
-            # if b == patternLength, means entire pattern that matches has been found
-            # append the index of the start of the pattern to the list
-            indexList.append(a-b+1)
-            b = lps[b-1]
-
-    #if not indexList:
-    #   print("Pattern not found")
-    #else:
-    #    print("Pattern found at Positions:", end=" ")
-    #    print(indexList)
-
-
-print(timeit.timeit('KMPAlgo(text, pattern)',
-                    'from __main__ import KMPAlgo, text, pattern', number=1000))
+length = len(pattern)
+characters = list(set(text))
+len_characters = len(characters)
+lps = [0]*length
+computeLPSArray(pattern, length, lps)
+FT = np.empty((len_characters,length))
+computeFailureTable(characters, pattern, lps, FT)
+printFailureTable(characters, FT)
